0d4f7a3249029235f008e730f44374932a48a7ca
thanh.dinhvan <thanh.dinhvan@vti.com.vn>
2023-02-01 10:55:16 +0700

    add search bar

--- lib/Home/home_page.dart
+++ lib/Home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mentalhealthapp/util/emotion_face.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -17,71 +18,142 @@ class _HomePageState extends State<HomePage> {
           padding: const EdgeInsets.all(25.0),
           child: Column(
             children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children:[
-                Text("Hi Thanh Van",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24, 
-                  fontWeight: FontWeight.bold),),
-                Text(
-                    "31-1-2023",
-                    style: TextStyle(color: Colors.blue[200]),
-                    )]
-              ),
-                
-                //Notification
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(12),
-                  color: Colors.blue[600]),
-                padding: EdgeInsets.all(16),
-                child: Icon(
-                  Icons.notifications,
-                  color: Colors.white ,),
-              )
-            ],),
-            SizedBox(
-              height: 10,
-            ),
-            // search bar
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.blue[600],
-                borderRadius: BorderRadius.circular(12)
-              ),
-              padding: EdgeInsets.all(10),
-              child: Row(
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Icon(Icons.search,
-                  color: Colors.white,),
-                  SizedBox(width: 8,),
-                  Text(
-                    "Search",
-                    style: TextStyle(
-                      color: Colors.white
+                  Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Hi Thanh Van",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          "31-1-2023",
+                          style: TextStyle(color: Colors.blue[200]),
+                        )
+                      ]),
+
+                  //Notification
+                  Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        color: Colors.blue[600]),
+                    padding: EdgeInsets.all(16),
+                    child: Icon(
+                      Icons.notifications,
+                      color: Colors.white,
                     ),
-                    )
+                  )
                 ],
               ),
-            ),
-            SizedBox(height: 25,),
-            Row(
-              children: [
-                Text("How do you feel ?",
+              SizedBox(
+                height: 10,
+              ),
+              // search bar
+              Container(
+                decoration: BoxDecoration(
+                    color: Colors.blue[600],
+                    borderRadius: BorderRadius.circular(12)),
+                padding: EdgeInsets.all(10),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.search,
+                      color: Colors.white,
+                    ),
+                    SizedBox(
+                      width: 8,
+                    ),
+                    Text(
+                      "Search",
+                      style: TextStyle(color: Colors.white),
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+                Text(
+                  "How do you feel ?",
                   style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 22, 
-                    fontWeight: FontWeight.bold),),
-                    Icon(Icons.more_horiz)
-              ]
-            ),
-            
-          ],),
+                      color: Colors.white,
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold),
+                ),
+                Icon(Icons.more_horiz)
+              ]),
+              SizedBox(
+                height: 25,
+              ),
+              Row(
+                children: [
+                  //bad
+                  Column(children: [
+                    EmoticonFace(
+                      emoticonFace: "😀",
+                    ),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    Text(
+                      "Bad",
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.bold),
+                    )
+                  ]),
+
+                  //fine
+                  Column(children: [
+                    EmoticonFace(
+                      emoticonFace: "😀",
+                    ),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    Text(
+                      "Bad",
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.bold),
+                    )
+                  ]),
+                  //well
+                  Column(children: [
+                    EmoticonFace(
+                      emoticonFace: "😀",
+                    ),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    Text(
+                      "Bad",
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.bold),
+                    )
+                  ]),
+                  //excellent
+                  Column(children: [
+                    EmoticonFace(
+                      emoticonFace: "😀",
+                    ),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    Text(
+                      "Bad",
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.bold),
+                    )
+                  ]),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
